e09525a5b1c7fbbc3f8f959dce4b03a72bd1a871
Nilomiranda <nilomiranda3@gmail.com>
2019-04-19 22:25:48 -0300

    Creating our first components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-const sum = (a,b) => a + b;
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
 
-alert(sum(1,5));
+class Button extends Component {
+    render () {
+        return <a href="">Click me bitch!</a>
+    }
+}
+
+class App extends Component {
+    render () {
+        return (
+            /**
+             * Fragment component act as a <div></div>
+             * but with no browser default styling
+             */
+            <Fragment>
+                <h1>Hello ReactJS</h1>
+                <Button />
+            </Fragment>
+        )
+    }
+}
+
+render(<App />, document.getElementById('app'))
