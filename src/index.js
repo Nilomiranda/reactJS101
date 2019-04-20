4f3c653d5eca523d189517caa50636e2fe7c59aa
Nilomiranda <nilomiranda3@gmail.com>
2019-04-19 22:50:02 -0300

    Setting default props and prop types

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom';
+import propTypes from 'prop-types'
 
 class Button extends Component {
-    render () {
-        return <a href="">Click me bitch!</a>
-    }
+  render () {
+    return <a href="" onClick={this.props.onClick}>{this.props.children}</a>
+  }
+}
+
+Button.defaultProps = {
+  children: 'Define me motherfucker!'
+}
+
+Button.propTypes = {
+  onClick: propTypes.func.isRequired,
+  children: propTypes.string
 }
 
 class App extends Component {
-    render () {
-        return (
-            /**
-             * Fragment component act as a <div></div>
-             * but with no browser default styling
-             */
-            <Fragment>
-                <h1>Hello ReactJS</h1>
-                <Button />
-            </Fragment>
-        )
-    }
+  handleClick() {
+    alert('You finally clicked me!! Thank you!!')
+  }
+
+  render () {
+    return (
+      /**
+       * Fragment component act as a <div></div>
+       * but with no browser default styling
+       */
+      <Fragment>
+        <h1>Hello ReactJS</h1>
+        <Button onClick={this.handleClick}>Now, are you clicking me bitch?</Button>
+        <Button />
+      </Fragment>
+    )
+  }
 }
 
 render(<App />, document.getElementById('app'))
